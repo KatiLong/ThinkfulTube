f5559dcf56ac2282c3352403f1c9a0628eb42e24
Kati Long <katileighlong@gmail.com>
2018-01-27 22:19:26 -0700

    Basic requirements met and basic CSS styling

--- Tube.js
+++ Tube.js
@@ -1,3 +1,9 @@
+// Other functions to add:
+// //click on thumbnails take to youtube video
+// //load more videos button
+// //add results section title "search results for ${searchTerm}"
+
+
 // AIzaSyBb3SPRWQyvuaXEj6Bp_8Erjz_qeRxha5g
 // "https://www.googleapis.com/youtube/v3/search"
 
@@ -5,15 +11,9 @@
 // key: (your API key as a string)
 // q: (your search term as a string)
 
-// buildApiRequest('GET',
-//                 '/youtube/v3/search',
-//                 {'maxResults': '25',
-//                  'part': 'snippet',
-//                  'q': 'surfing',
-//                  'type': ''});
-
 // URL variable
 const YOUTUBE_SEARCH_URL = 'https://www.googleapis.com/youtube/v3/search';
+const videoIDs = [];
 // get API data function which makes JSON/AJAX call - URL, query, callback params
 function getApiData (searchTerm, callback) {
   //create API object [inside getApiData]
@@ -27,20 +27,25 @@ function getApiData (searchTerm, callback) {
 }
 
 // function that generates results string
-function generateResults (elem) {
+function generateResults (elem, index) {
   console.log(`Results string generated here`);
+  // console.log(elem);
+  videoIDs.push(`${elem.id.videoId}`)
   return `
-    <div class ="thumbnail-div">
+    <div class ="thumbnail-div ${index}">
       <img src="${elem.snippet.thumbnails.medium.url}" class="thumbnail-img">
-      <h3>Thumbnail Title</h3>
+      <h3 class='video-title'>${elem.snippet.title}</h3>
+      <p></p>
     </div>`;
 }
 
 function displayResults (data) {
   console.log(`Display Results ran`);
   console.log(data);
-  const results = data.items.map((elem, index) => generateResults(elem))
+  const results = data.items.map((elem, index) => generateResults(elem, index));
   $('.js-search-results').html(results);
+  console.log(videoIDs);
+  console.log(results);
 }
 
 // submit event listener function (where results appended to page?)
@@ -58,6 +63,18 @@ function submitListen (){
   })
 }
 
+// add event listener for user clicking on thumbnails, takes to video
+// click listener on thumnail image, title
+// open new tab with link
+
+function videoURL () {
+  $('.js-search-results').on('click', '.thumbnail-img', function (event){
+    event.stopPropogation();
+    let video = $(event.currentTarget).parent();
+    window.open(event.currentTarget, '_blank');
+  })
+}
+
 //user stories
 
 // URL variable
@@ -68,4 +85,4 @@ function submitListen (){
 // submit event listener function (where results appended to page?)
 // document ready function
 
-$(submitListen);
+$(submitListen());
