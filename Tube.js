7305014eb49794735b1b162822c048cb8ea78158
Kati Long <katileighlong@gmail.com>
2018-01-30 13:37:01 -0700

    Cleaned up JS file

--- Tube.js
+++ Tube.js
@@ -1,29 +1,17 @@
 // Other functions to add:
-// //click on thumbnails take to youtube video
-// //load more videos button
 // //add results section title "search results for ${searchTerm}"
-
-
-// AIzaSyBb3SPRWQyvuaXEj6Bp_8Erjz_qeRxha5g
-// "https://www.googleapis.com/youtube/v3/search"
-
-// part: 'snippet'
-// key: (your API key as a string)
-// q: (your search term as a string)
-
-// URL variable
-const YOUTUBE_SEARCH_URL = 'https://www.googleapis.com/youtube/v3/search';
 
 // get API data function which makes JSON/AJAX call - URL, query, callback params
 function getApiData (searchTerm, callback) {
   //create API object [inside getApiData]
   console.log(`get API data function ran`)
-  const query = {
+  $.getJSON('https://www.googleapis.com/youtube/v3/search', {
     part: 'snippet',
+    maxResults: 20,
     key: 'AIzaSyBb3SPRWQyvuaXEj6Bp_8Erjz_qeRxha5g',
     q: searchTerm
-  }
-  $.getJSON(YOUTUBE_SEARCH_URL, query, callback);
+    },
+    callback);
 }
 
 // function that generates results string
@@ -57,8 +45,6 @@ function displayResults (data) {
   $('.js-search-results').html(results);
 }
 
-// submit event listener function (where results appended to page?)
-// invokve get API data function
 function submitListen (){
   console.log(`the submitListen function ran`);
   $('.js-search-form').submit(event => {
@@ -68,43 +54,11 @@ function submitListen (){
     const userText = queryTarget.val();
     // clear out the input
     queryTarget.val("");
-    $('.js-search-results').prop('hidden', false);
+    $('main').prop('hidden', false);
     getApiData(userText, displayResults);
     // displayResults();
   })
 }
 
-// add event listener for user clicking on thumbnails, takes to video
-// click listener on thumnail image, title
-// open new tab with link
-
-
 
 $(submitListen());
-
-
-//user stories
-
-// URL variable
-// get API data function which makes JSON/AJAX call - URL, query, callback params
-  //create API object [inside getApiData]
-// function that generates results string
-// diplay search data (videos as thumbnails)
-// submit event listener function (where results appended to page?)
-// document ready function
-
-
-/* Passing Local Data between Functions */
-// const getData = doIt();
-
-// doThat(getData);
-
-// function doIt() {
-  // const person = { name: "Kati", age: 19 };
-
-  // function getObj() {
-    // return person;
-  // } return getObj;
-// }
-
-// function doThat(dataFn) { console.log(dataFn()); }
